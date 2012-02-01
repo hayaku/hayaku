50ddf75233c13e68e9640f24536747a4f836541c
kizu <kizmarh@gmail.com>
2012-02-01 15:34:41 +0400

    Added prefix for the preference

--- hayaku.py
+++ hayaku.py
@@ -51,7 +51,7 @@ class HayakuCommand(sublime_plugin.TextCommand):
         if not extracted[0]:
             return
         # print extracted, '2'
-        template = make_template(self.view.settings().get("whitespace_after_colon"), *extracted)
+        template = make_template(self.view.settings().get("hayaku_whitespace_after_colon"), *extracted)
         new_cur_pos = cur_pos-len(abbr)
         assert cur_pos-len(abbr) >= 0
         self.view.erase(edit, sublime.Region(new_cur_pos, cur_pos))
