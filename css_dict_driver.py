da84908be558b88d69f418bef300dc35044b2935
Sergey Mezentsev <thebits@yandex.ru>
2012-02-17 23:18:21 +0400

    Поменял путь к файлу обратно, без директории

--- css_dict_driver.py
+++ css_dict_driver.py
@@ -2,7 +2,8 @@
 
 from itertools import product
 
-CSS_DICT_FILENAME = 'core/CSS-dict.txt'
+# CSS_DICT_FILENAME = 'core/CSS-dict.txt'
+CSS_DICT_FILENAME = 'CSS-dict.txt'
 
 # парсер формата файла с css-правилами
 
