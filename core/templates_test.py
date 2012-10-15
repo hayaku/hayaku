0b12ea07ce9ef1d39565d4de041193da5d602922
Sergey Mezentsev <thebits@yandex.ru>
2012-10-15 11:07:54 +0400

    Добавлены опции

--- core/templates_test.py
+++ core/templates_test.py
@@ -5,6 +5,12 @@ from templates import expand_value
 from probe import extract
 
 class ExpandValueTests(unittest.TestCase):
+    def setUp(self):
+        self.opts = {
+           'hayaku_CSS_default_unit': 'px',
+           'hayaku_CSS_default_unit_decimal': 'em',
+        }
+
     def test_0(self):
         self.assertEqual(expand_value(extract('z8')), '8')
 
@@ -12,22 +18,22 @@ class ExpandValueTests(unittest.TestCase):
         self.assertEqual(expand_value(extract('zi12')), '12')
 
     def test_2(self):
-        self.assertEqual(expand_value(extract('w0')), '0')
+        self.assertEqual(expand_value(extract('w0'), self.opts), '0')
 
     def test_3(self):
-        self.assertEqual(expand_value(extract('w0p')), '0')
+        self.assertEqual(expand_value(extract('w0p'), self.opts), '0')
 
     def test_4(self):
-        self.assertEqual(expand_value(extract('w0px')), '0')
+        self.assertEqual(expand_value(extract('w0px'), self.opts), '0')
 
     def test_5(self):
-        self.assertEqual(expand_value(extract('w')), '100%')
+        self.assertEqual(expand_value(extract('w'), self.opts), '100%')
 
     def test_6(self):
         self.assertEqual(expand_value(extract('w10%')), '10%')
 
     def test_7(self):
-        self.assertEqual(expand_value(extract('w10perc')), '10%')
+        self.assertEqual(expand_value(extract('w10perc'), self.opts), '10%')
 
     def test_8(self):
         self.assertEqual(expand_value(extract('vat')), 'top')
