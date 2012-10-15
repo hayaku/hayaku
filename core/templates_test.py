c5d177bd693e1348b694fc119f4bc7a7bca14c33
Sergey Mezentsev <thebits@yandex.ru>
2012-10-15 10:50:02 +0400

    Рефакторинг тестов

--- core/templates_test.py
+++ core/templates_test.py
@@ -2,27 +2,23 @@
 import unittest
 
 from templates import expand_value
+from probe import extract
 
 class ExpandValueTests(unittest.TestCase):
     def test_0(self):
-        args = {'property-name': 'zoom', 'important': False, 'abbr': 'z8', 'type-value': 8}
-        self.assertEqual(expand_value(args), '8')
+        self.assertEqual(expand_value(extract('z8')), '8')
 
     def test_1(self):
-        args = {'property-name': 'z-index', 'important': False, 'abbr': 'zi2', 'type-value': 2}
-        self.assertEqual(expand_value(args), '2')
+        self.assertEqual(expand_value(extract('zi12')), '12')
 
     def test_2(self):
-        args = {'property-name': 'width', 'default-value': '100%', 'important': False, 'abbr': 'w0', 'type-value': 0}
-        self.assertEqual(expand_value(args), '0')
+        self.assertEqual(expand_value(extract('w0')), '0')
 
     def test_3(self):
-        args = {'property-name': 'width', 'default-value': '100%', 'important': False, 'abbr': 'w0p', 'type-value': 0, 'type-name': 'p'}
-        self.assertEqual(expand_value(args), '0')
+        self.assertEqual(expand_value(extract('w0p')), '0')
 
     def test_4(self):
-        args = {'property-name': 'width', 'default-value': '100%', 'important': False, 'abbr': 'w0px', 'type-value': 0, 'type-name': 'px'}
-        self.assertEqual(expand_value(args), '0')
+        self.assertEqual(expand_value(extract('w0px')), '0')
 
 
 if __name__ == '__main__':
