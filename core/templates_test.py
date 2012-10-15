6b8f8f8c98a55b414e95dcd39ab9c519fbe535ed
Sergey Mezentsev <thebits@yandex.ru>
2012-10-15 10:44:28 +0400

    Добавлены тесты для #111

--- core/templates_test.py
+++ core/templates_test.py
@@ -12,6 +12,18 @@ class ExpandValueTests(unittest.TestCase):
         args = {'property-name': 'z-index', 'important': False, 'abbr': 'zi2', 'type-value': 2}
         self.assertEqual(expand_value(args), '2')
 
+    def test_2(self):
+        args = {'property-name': 'width', 'default-value': '100%', 'important': False, 'abbr': 'w0', 'type-value': 0}
+        self.assertEqual(expand_value(args), '0')
+
+    def test_3(self):
+        args = {'property-name': 'width', 'default-value': '100%', 'important': False, 'abbr': 'w0p', 'type-value': 0, 'type-name': 'p'}
+        self.assertEqual(expand_value(args), '0')
+
+    def test_4(self):
+        args = {'property-name': 'width', 'default-value': '100%', 'important': False, 'abbr': 'w0px', 'type-value': 0, 'type-name': 'px'}
+        self.assertEqual(expand_value(args), '0')
+
 
 if __name__ == '__main__':
     unittest.main()
