86c1c6a09e5832c1be7ed9e239b09ba5271a8089
kizu <kizmarh@gmail.com>
2012-11-23 21:44:58 +0400

    Remove unused code for HayakuChangeNumberCommand (would be there in history anyway)

--- hayaku.py
+++ hayaku.py
@@ -11,7 +11,6 @@ from core.templates import make_template
 
 __all__ = [
     'HayakuCommand',
-    'HayakuChangeNumberCommand',
 ]
 
 # максимальный размер css properties
@@ -110,82 +109,3 @@ class HayakuAddLineCommand(sublime_plugin.TextCommand):
             self.view.erase(edit, reg)
         else:
             self.view.run_command('insert', {"characters": "\n"})
-
-
-OPERATION_TABLE = {
-    "up": partial(operator.add, 1),
-    "down": partial(operator.add, -1),
-    "shift_up": partial(operator.add, 10),
-    "shift_down": partial(operator.add, -10),
-    "alt_up": partial(operator.add, 0.1),
-    "alt_down": partial(operator.add, -0.1),
-}
-
-# Изменяет число по сочетаниям ctrl/alt/shift + up/down
-class HayakuChangeNumberCommand(sublime_plugin.TextCommand):
-    def run(self, edit, key):
-
-        # поиск текущей позиции в файле
-        regions = self.view.sel()
-        if len(regions) > 1:
-            # разобраться с многооконными выборками
-            # пока что работаем только с одним регионом
-            for r in regions:
-                self.view.insert(edit, r, '\t')
-            return
-        region = regions[0]
-        if not region.empty():
-            # сделать работы с выделенным словом
-            self.view.insert(edit, region, '\t')
-            return
-        cur_pos = region.begin()
-
-        # Буферы для чисел до и после курсора
-        before_buf = []
-        after_buf = []
-
-        # считывает линию и текущую позицию в куросора в строке
-        line_region = self.view.line(cur_pos)
-        line = self.view.substr(line_region)
-        row, col = self.view.rowcol(cur_pos)
-
-        for i in range(col-1, 0-1, -1):
-            if line[i].isdigit() or line[i] in ('.', '-'):
-                before_buf.append(line[i])
-            else:
-                break
-        before_buf = before_buf[::-1]
-        for i in range(col, len(line)):
-            if line[i].isdigit() or line[i] in ('.', '-'):
-                after_buf.append(line[i])
-            else:
-                break
-
-        start_pos_offset = len(before_buf)
-        end_pos_offset = len(after_buf)
-
-        # прочитать число
-        total_buf = before_buf + after_buf
-        buf = u''.join(total_buf)
-        value = None
-        try:
-            value = float(buf)
-            value = int(buf)
-        except ValueError:
-            if value is None:
-                return
-
-        # Расчёт нового значения
-        operation = OPERATION_TABLE[key]
-        new_value = operation(value)
-
-        # Замена региона с числом
-        start_pos = cur_pos - start_pos_offset
-        end_pos = cur_pos + end_pos_offset
-        replace_region = sublime.Region(start_pos, end_pos)
-        self.view.replace(edit, replace_region, str(new_value))
-
-        # установить курсор на место
-        self.view.sel().clear()
-        self.view.sel().add(sublime.Region(cur_pos, cur_pos))
-
