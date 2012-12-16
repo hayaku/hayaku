10b4d1065543b34ff149fab0b023d298cb6d9424
Sergey Mezentsev <thebits@yandex-team.ru>
2012-12-16 14:48:13 +0400

    migrate

--- core/run_test.sh
+++
@@ -1,6 +0,0 @@
-python hayaku_test.py 2> test.txt
-python segmentation_test.py 2>> test.txt
-python templates_test.py 2>> test.txt
-python semantic_test.py 2>> test.txt
-# python formatter.py
-grep FAILED test.txt
