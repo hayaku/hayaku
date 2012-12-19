bdaf117ca50f02c35bde4ecd54c7ca78db2606e3
kizu <kizmarh@gmail.com>
2012-12-19 21:34:54 +0400

    minor fix for bug in color postexpan

--- core/templates.py
+++ core/templates.py
@@ -357,7 +357,7 @@ def make_template(args, options):
                     value = ''
                     # Insert hash and doubling letters
                     snippet_parts['before'].append({
-                        "match":  "[0-9a-fA-F]{1,6}[^,\.]*$",
+                        "match":  "[0-9a-fA-F]{1,6}\s*(!\w*\s*)?$",
                         "insert": "#"
                         })
                     snippet_parts['after'].append({
