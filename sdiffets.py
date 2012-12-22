bec80937d52e448552aca4d673b0c9b40c6c4ad2
kizu <kizmarh@gmail.com>
2012-12-22 14:24:24 +0400

    Added basic splitting

--- sdiffets.py
+++ sdiffets.py
@@ -16,10 +16,30 @@ class HayakuSdiffetsCommand(sublime_plugin.TextCommand):
         is_single_caret = snippet_input.a == snippet_input.b
         if is_single_caret:
             snippet_input = self.view.full_line(regions[0])
-            cursor_offset = regions[0].a - snippet_input.a
+            initial_offset = regions[0].a - snippet_input.a
+            cursor_offset = initial_offset
 
         snippet_content = self.view.substr(snippet_input)
+        snippet_parts = re.split('(\W+)',snippet_content) # the split regex is just a placehoder, would be more complex
 
+        count = 2 # for adding the initial tabstop to mimic the cloning behavior, could be configurable
+        count_offset = 0
+
+        for index, part in enumerate(snippet_parts):
+            if re.match('^\w+$',part):
+                snippet_parts[index] = '${' + str(count) + ':' + part + '}'
+                count += 1
+
+                if is_single_caret and initial_offset > count_offset:
+                    cursor_offset += len(snippet_parts[index]) - len(part)
+                    if initial_offset < count_offset + len(part):
+                        cursor_offset -= 1
+
+            count_offset += len(part)
+
+        snippet_content = ''.join(snippet_parts)
+
+        # Finding a place where to insert the clone
         target = sublime.Region(max(snippet_input.a,snippet_input.b))
 
         # Creating a new line — without it there are strange things happening with the indents
@@ -33,10 +53,10 @@ class HayakuSdiffetsCommand(sublime_plugin.TextCommand):
             # the offset would be changed according to the added snippets
             snippet_result = snippet_content[:cursor_offset] + '$1' + snippet_content[cursor_offset:]
         else:
-            snippet_result = "${1:" + re.sub('\$','\$',re.sub('}','\}',snippet_content)) + "}"
+            snippet_result = "${1:" + snippet_content + "}"
 
         self.view.run_command("insert_snippet", {"contents": snippet_result})
 
         # Removing an extra newline that were there for indent behavior fixing
-        self.view.erase(edit, sublime.Region(target.a + len(snippet_content),target.a + len(snippet_content) + 1))
+        self.view.erase(edit, sublime.Region(target.a + count_offset, target.a + count_offset + 1))
 
