11ca861974042aff67407649cdb448e1f06caeef
Roman Komarov <kizmarh@ya.ru>
2014-02-05 17:46:48 +0400

    Refactored cycling to use object for value

--- hayaku_cycling.py
+++ hayaku_cycling.py
@@ -34,9 +34,7 @@ class HayakuCyclingThroughValuesCommand(sublime_plugin.TextCommand):
             self.region = region
             self.region_index = index
             self.new_value = None
-            self.current_value = None
-            self.current_value_prop = None
-            self.current_value_region = None
+            self.current_value = {}
 
             # Check if the current region was in the area where the first one made changes to
             should_proceed = not any(dirty_region.intersects(region) for dirty_region in self.dirty_regions)
@@ -73,13 +71,13 @@ class HayakuCyclingThroughValuesCommand(sublime_plugin.TextCommand):
             return False
 
         old_position = self.view.sel()[self.region_index]
-        new_position = self.get_new_position(old_position, self.current_value_region, self.new_value)
+        new_position = self.get_new_position(old_position, self.current_value.get('region'), self.new_value)
 
         if old_position != new_position:
             self.view.sel().subtract(old_position)
 
-        self.view.replace(self.edit, self.current_value_region, self.new_value)
-        self.dirty_regions.append(self.current_value_region)
+        self.view.replace(self.edit, self.current_value.get('region'), self.new_value)
+        self.dirty_regions.append(self.current_value.get('region'))
 
         if old_position != new_position:
             self.view.sel().add(new_position)
@@ -120,7 +118,7 @@ class HayakuCyclingThroughValuesCommand(sublime_plugin.TextCommand):
         return result, result_index
 
     def get_current_CSS_value(self):
-        if self.current_value or not sublime.score_selector(self.view.scope_name(self.region.a), 'source.css, source.less, source.sass, source.scss, source.stylus'):
+        if self.current_value.get('value') or not sublime.score_selector(self.view.scope_name(self.region.a), 'source.css, source.less, source.sass, source.scss, source.stylus'):
             return False
 
         # TODO: think on the get_closest_value to accept Region
@@ -148,12 +146,13 @@ class HayakuCyclingThroughValuesCommand(sublime_plugin.TextCommand):
             )
 
         if value:
-            self.current_value = value
-            self.current_value_region = sublime.Region(value_index, value_index + len(value))
-            self.current_value_prop = parsed_declaration.group(3)
+            self.current_value['context'] = 'CSS value'
+            self.current_value['value'] = value
+            self.current_value['region'] = sublime.Region(value_index, value_index + len(value))
+            self.current_value['prop'] = parsed_declaration.group(3)
 
     def get_current_numeric_value(self):
-        if self.current_value:
+        if self.current_value.get('value'):
             return False
 
         # TODO: make the get_closest_value to return Region
@@ -172,17 +171,18 @@ class HayakuCyclingThroughValuesCommand(sublime_plugin.TextCommand):
             )
 
         if number:
-            self.current_value = number
-            self.current_value_region = sublime.Region(number_index, number_index + len(number))
+            self.current_value['context'] = 'Number'
+            self.current_value['value'] = number
+            self.current_value['region'] = sublime.Region(number_index, number_index + len(number))
 
 
     def rotate_CSS_string(self):
-        if self.new_value or not (self.current_value and self.current_value_prop):
+        if self.new_value or not (self.current_value.get('value') and self.current_value.get('prop')):
             return False
 
-        props_values = get_values_by_property(self.current_value_prop)
-        if self.current_value in props_values:
-            index = props_values.index(self.current_value)
+        props_values = get_values_by_property(self.current_value.get('prop'))
+        if self.current_value.get('value') in props_values:
+            index = props_values.index(self.current_value.get('value'))
             if self.modifier > 0:
                 index += 1
             elif self.modifier < 0:
@@ -191,14 +191,14 @@ class HayakuCyclingThroughValuesCommand(sublime_plugin.TextCommand):
             self.new_value = props_values[index % len(props_values)]
 
     def rotate_numeric_value(self):
-        if self.new_value or not self.current_value:
+        if self.new_value or not self.current_value.get('value'):
             return False
 
         left_limit = float("-inf")
-        if self.current_value_prop:
-            if get_key_from_property(self.current_value_prop, 'always_positive'):
+        if self.current_value.get('prop'):
+            if get_key_from_property(self.current_value.get('prop'), 'always_positive'):
                 left_limit = float(0)
 
-        found_number = re.search(r'^(-?\d*\.?\d+)(.*)$', self.current_value)
+        found_number = re.search(r'^(-?\d*\.?\d+)(.*)$', self.current_value.get('value'))
         if found_number:
             self.new_value = str(max(left_limit, round(float(found_number.group(1)) + self.modifier, 13))).rstrip('0').rstrip('.') + found_number.group(2)
